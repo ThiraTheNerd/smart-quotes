84a9b67740a1b2ec647eee1ca0cc09d4b3568f44
ThiraTheNerd <thiragithinji@gmail.com>
2021-05-16 14:52:37 +0300

    add the Quote class

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+
 
   constructor() { }
 
