7a2d0e24ee404ecaffeb9f11f39ee7906d266bd7
ThiraTheNerd <thiragithinji@gmail.com>
2021-05-16 22:31:53 +0300

    add the date ago pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { PipesPipe } from './pipes.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    PipesPipe
   ],
   imports: [
     BrowserModule
