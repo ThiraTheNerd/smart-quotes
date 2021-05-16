ebef577e08e96f4697cc2dbf01fe01d2f7048e0c
ThiraTheNerd <thiragithinji@gmail.com>
2021-05-16 16:46:28 +0300

    add a class Quote and define the variables

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,16 @@
 export class Quote {
+  quotename: string;
+  author: string;
+  votes: number;
+  name: string;
+
+
+  constructor(quotename: string, author:string, name:string, votes?: number){
+    this.quotename = quotename;
+    this.author= author;
+    this.name = name;
+    this.votes = votes || 0;
+
+  }
 }
+
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit, HostBinding } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  // @HostBinding('attr.class') cssClass = "row";
+  votes: number;
+  quote: string;
+  author: string;
+  name : string;
 
+  constructor() {
+    this.quote = `This is a wider card with supporting text below as a natural lead-in to it.`;
+    this.author = 'Jeremy Williams';
+    this.name = 'Mercy'
+    this.votes = 0
+   }
 
-
-  constructor() { }
+  voteUp(): boolean{
+    this.votes += 1;
+    return false;
+  }
+  voteDown(): boolean{
+    this.votes -= 1;
+    return false;
+  }
 
   ngOnInit() {
   }
